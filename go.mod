def64355688371f429cd85bda26f1399f6af9eb4
Alex Denisov <alex.a.denisov@gmail.com>
2024-06-17 00:07:42 -0400

    initial import

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/andsv/go-xl_test
+
+go 1.22.2
+
+require github.com/adnsv/go-xl v0.1.0
+
+require (
+	github.com/adnsv/srw v0.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/adnsv/go-xl/xl"
+)
+
+func main() {
+
+	fmt.Printf("Generating XLSX file\n")
+
+	wb := xl.NewWorkbook()
+	wb.AppName = "My App"
+
+	sheet, err := wb.AddSheet("sheet1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	row := sheet.AddRow()
+	row.AddCell().SetStr("col1")
+	row.AddCell().SetStr("col2")
+	row.AddCell().SetStr("col3")
+
+	row = sheet.AddRow()
+	row.AddCell().SetInt(1)
+	row.AddCell().SetInt(2)
+	row.AddCell().SetInt(3)
+
+	row = sheet.AddRow()
+	row.Height = 64
+	{
+		fn := "./testdata/image1.png"
+		blob, err := os.ReadFile(fn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		row.AddCell().SetPicture(&xl.PictureInfo{
+			Extension: filepath.Ext(fn),
+			Blob:      blob,
+		})
+	}
+	{
+		fn := "./testdata/image2.jpeg"
+		blob, err := os.ReadFile(fn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		row.AddCell().SetPicture(&xl.PictureInfo{
+			Extension: filepath.Ext(fn),
+			Blob:      blob,
+		})
+	}
+
+	debugdir := "./testdata/dbg"
+	fmt.Printf("Writing file parts into %s\n", debugdir)
+	ds := xl.NewDirStorage("./testdata/dbg")
+	dw := xl.NewWriter(ds)
+	err = dw.Write(wb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outfn := "./testdata/dbg.xlsx"
+	fmt.Printf("Writing %s\n", outfn)
+	f, err := os.Create(outfn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	zs := xl.NewZipStorage(f)
+	zw := xl.NewWriter(zs)
+	defer zs.Close()
+	zw.Write(wb)
+
+	fmt.Printf("Mission Accomplished\n")
+}
